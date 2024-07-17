2e6e6f43ed5fd76ab7738f2f6ea1af82080734eb
KayDeeGee <kylebryanbasco@gmail.com>
2024-07-17 13:15:11 +0800

    ewan

--- resources/js/sections/AboutMe.jsx
+++ resources/js/sections/AboutMe.jsx
@@ -21,7 +21,7 @@ function AboutMe({ className }) {
         <div className={`${className} d-flex flex-column h-100`}>
             <div className="d-flex flex-column align-items-center h-100">
                 <div className="my-img"></div>
-                <h1>Kyle Bryan basco</h1>
+                <h1>Kyle Bryan Basco</h1>
                 {roles.map((role, index) => (
                     <h2 key={index} className={`slider-text ${currentRole === index ? 'active' : ''} accent-color`}>
                         {role}
@@ -31,7 +31,7 @@ function AboutMe({ className }) {
             </div>
             <div className="mt-auto mb-3 text-center">
                 <h6>Contacts</h6>
-                <p>0992-530-5430</p>
+                <p>0992-350-5430</p>
                 <p>
                     bascokylebryan@gmail.com
                 </p>
